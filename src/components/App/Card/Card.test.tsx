f159ca37e4a7b864f3315da260c9bd46f659c104
ShubhamS021 <shubham@urbanoinfotech.com>
2023-02-13 15:05:12 +0100

    test: added branches for task and card

--- src/components/App/Card/Card.test.tsx
+++ src/components/App/Card/Card.test.tsx
@@ -1,11 +1,21 @@
-import { render, screen } from '@testing-library/react'
-import { CardComponent } from './Card'
+import { render, screen } from '@testing-library/react';
+import { CardComponent } from './Card';
 
-test('renders basic setup message', () => {
-    render(<CardComponent />)
-    const title = screen.getByText(/card title/i)
-    expect(title).toBeInTheDocument()
+test('renders basic card', () => {
+    render(
+        <CardComponent title={'card title'} description="card description" />
+    );
+    const title = screen.getByText(/card title/i);
+    expect(title).toBeInTheDocument();
 
-    const description = screen.getByText(/description/i)
-    expect(description).toBeInTheDocument()
-})
+    const description = screen.getByText(/description/i);
+    expect(description).toBeInTheDocument();
+});
+
+test('breaks render on card with empty title', () => {
+    try {
+        render(<CardComponent title={''} />);
+    } catch (e) {
+        expect(e.message).toBe('no title set');
+    }
+});
--- src/components/App/Task/Task.spec.tsx
+++ src/components/App/Task/Task.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { TaskComponent } from './Task';
 
@@ -13,6 +13,15 @@ test('renders unchecked basic task', () => {
     expect(uncheckedTaskCheckbox).not.toBeChecked();
 });
 
+test('renders checked basic task without prop', () => {
+    render(<TaskComponent description="test unchecked task" />);
+    const uncheckedTask = screen.getByText(/test unchecked task/i);
+    expect(uncheckedTask).toBeInTheDocument();
+
+    const uncheckedTaskCheckbox = screen.getByTestId('input-checkbox');
+    expect(uncheckedTaskCheckbox).not.toBeChecked();
+});
+
 test('renders checked basic task', () => {
     render(<TaskComponent description="test checked task" fulfilled={true} />);
     const checkedTask = screen.getByText(/test checked task/i);
@@ -20,4 +29,19 @@ test('renders checked basic task', () => {
 
     const checkedTaskCheckbox = screen.getByTestId('input-checkbox');
     expect(checkedTaskCheckbox).toBeChecked();
+});
+
+test('ticks a task', () => {
+    render(
+        <TaskComponent
+            description="test unchecked task to be checked"
+            fulfilled={false}
+        />
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    const checkBox = screen.getByTestId('input-checkbox') as HTMLInputElement;
+    expect(checkBox.checked).toBeFalsy();
+    fireEvent.click(checkBox);
+    expect(checkBox.checked).toBeTruthy();
 });

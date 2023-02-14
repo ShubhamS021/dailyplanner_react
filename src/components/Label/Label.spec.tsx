6c9b98c4b48273193432548a112f4734a7b9ce2f
ShubhamS021 <shubham@urbanoinfotech.com>
2023-02-14 12:38:31 +0100

    test: import adjustments

--- src/components/Label/Label.spec.tsx
+++ src/components/Label/Label.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { colors } from '../../../theme/colors';
+import { colors } from '../../theme/colors';
 
 import { LabelComponent } from './Label';
 
--- src/components/Lane/Lane.spec.tsx
+++ src/components/Lane/Lane.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { colors } from '../../../theme/colors';
+import { colors } from '../../theme/colors';
 import { LaneComponent } from './Lane';
 
 test('renders basic lane', () => {

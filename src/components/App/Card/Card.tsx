e65c86afb941ca81919d08c86fa731a191e8a209
ShubhamS021 <shubham@urbanoinfotech.com>
2023-02-14 11:09:17 +0100

    refactor: card details to lane (first layer up, next will be cards as prop for lane)

--- src/components/App/Card/Card.tsx
+++ src/components/App/Card/Card.tsx
@@ -1,71 +1,67 @@
 import { type Tag } from 'interfaces/Tag';
 import { type Task } from 'interfaces/Task';
-import { colors } from '../../../theme/colors';
 import { TagComponent } from '../Tag/Tag';
 import { TaskComponent } from '../Task/Task';
 
 export interface CardProps {
     title: string;
     description?: string;
+    upperTags?: Tag[];
+    lowerTags?: Tag[];
+    tasks?: Task[];
 }
 
-const upperTags: Tag[] = [
-    { id: 1, text: 'Tag A', color: colors.rose },
-    { id: 2, text: 'Tag B', color: colors.green },
-];
+export const CardComponent: React.FC<CardProps> = ({
+    title,
+    description,
+    upperTags,
+    lowerTags,
+    tasks,
+}) => {
+    if (title === '') throw new Error('no title set');
 
-const lowerTags: Tag[] = [
-    { id: 1, text: 'Tag C', color: colors.rose },
-    { id: 2, text: 'Tag D', color: colors.green },
-];
+    const renderTags = (tags: Tag[] | undefined) => {
+        if (tags === undefined) return;
+        return (
+            <>
+                <div className="flex gap-1">
+                    {tags.map((t) => (
+                        <TagComponent
+                            key={t.id}
+                            color={t.color}
+                            text={t.text}
+                        />
+                    ))}
+                </div>
+            </>
+        );
+    };
 
-const tasks: Task[] = [
-    { id: 1, description: 'Task 1', fulfilled: false },
-    { id: 2, description: 'Task 2', fulfilled: true },
-    { id: 3, description: 'Task 3', fulfilled: false },
-];
-
-export const renderTags = (tags: Tag[]) => {
-    return (
-        <>
-            <div className="flex gap-1">
-                {tags.map((t) => (
-                    <TagComponent key={t.id} color={t.color} text={t.text} />
+    const renderTasks = (tasks: Task[] | undefined) => {
+        if (tasks === undefined) return;
+        return (
+            <>
+                {tasks.map((t) => (
+                    <TaskComponent
+                        key={t.id}
+                        description={t.description}
+                        fulfilled={t.fulfilled}
+                    />
                 ))}
-            </div>
-        </>
-    );
-};
-
-export const renderTasks = (tasks: Task[]) => {
-    return (
-        <>
-            {tasks.map((t) => (
-                <TaskComponent
-                    key={t.id}
-                    description={t.description}
-                    fulfilled={t.fulfilled}
-                />
-            ))}
-        </>
-    );
-};
-
-export const CardComponent = (props: CardProps) => {
-    if (props.title === '') throw new Error('no title set');
+            </>
+        );
+    };
 
     return (
         <div className="bg-white border border-solid rounded-lg border-[#DDDDDD] p-4 drop-shadow-[0_2px_4px_rgba(0,0,0,0.08)]">
             <div className="flex flex-col gap-2 items-start">
-                {upperTags.length > 0 && renderTags(upperTags)}
-                <h3 className="font-semibold text-base">{props.title}</h3>
-                {props.description !== '' && (
-                    <p className="text-sm text-[#5A5A65]">
-                        {props.description}
-                    </p>
+                {renderTags(upperTags)}
+                <h3 className="font-semibold text-base">{title}</h3>
+                {description !== '' && (
+                    <p className="text-sm text-[#5A5A65]">{description}</p>
                 )}
-                {tasks.length > 0 && renderTasks(tasks)}
-                {lowerTags.length > 0 && renderTags(lowerTags)}
+                {renderTasks(tasks)}
+                {renderTags(lowerTags)}
             </div>
         </div>
     );
--- src/components/App/Lane/Lane.tsx
+++ src/components/App/Lane/Lane.tsx
@@ -1,3 +1,5 @@
+import { colors } from 'theme/colors';
+import { type Card } from '../../../interfaces/Card';
 import { CardComponent } from '../Card/Card';
 import { LabelComponent } from '../Label/Label';
 
@@ -7,21 +9,50 @@ export interface LaneProps {
 }
 
 export const LaneComponent = (props: LaneProps) => {
+    const cards: Card[] = [
+        {
+            id: 1,
+            title: 'Working title',
+            description: 'Some text',
+            upperTags: [
+                { id: 1, text: 'Tag A', color: colors.rose },
+                { id: 2, text: 'Tag B', color: colors.green },
+            ],
+            lowerTags: [
+                { id: 1, text: 'Tag C', color: colors.rose },
+                { id: 2, text: 'Tag D', color: colors.green },
+            ],
+            tasks: [
+                { id: 1, description: 'Task 1' },
+                { id: 2, description: 'Task 2' },
+            ],
+        },
+    ];
+
+    const renderCards = (cards: Card[] | undefined) => {
+        if (cards === undefined) return;
+        return (
+            <>
+                {cards.map((c, index) => (
+                    <CardComponent
+                        key={index}
+                        title={c.title}
+                        description={c.description}
+                        upperTags={c.upperTags}
+                        tasks={c.tasks}
+                        lowerTags={c.lowerTags}
+                    />
+                ))}
+            </>
+        );
+    };
+
     return (
         <div className="flex flex-col gap-2">
-            <LabelComponent color={props.color} text={props.text} />
-            <CardComponent
-                title="Card Title"
-                description="A description of a task."
-            />
-            <CardComponent
-                title="Card Title"
-                description="A description of a task."
-            />
-            <CardComponent
-                title="Card Title"
-                description="A description of a task."
-            />
+            <div className="w-fit">
+                <LabelComponent color={props.color} text={props.text} />
+            </div>
+            {renderCards(cards)}
         </div>
     );
 };

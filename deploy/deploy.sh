35713d83d89e64534cd0d3a6fc5c835f80dfeb91
ShubhamS021 <shubham@urbanoinfotech.com>
2023-06-16 14:04:51 +0200

    build: switched target to hosting127372 build: correction of function references

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+environment_name=$ENVIRONMENT_NAME
 DEPLOY_SERVER=$DEPLOY_SERVER
 DEPLOY_USER=$DEPLOY_USER
-SERVER_FOLDER="dayplanner"
 
-# Building React output
+if [[ "$environment_name" == "qa" ]]; then
+  SERVER_FOLDER="httpdocs/apps-qa/plannerapp"
+elif [[ "$environment_name" == "prod" ]]; then
+  SERVER_FOLDER="httpdocs/apps/plannerapp"
+else
+  echo "Invalid environment name: $environment_name"
+  exit 1
+fi
+
 yarn install
 yarn build
 
-echo "Deploying to ${DEPLOY_SERVER}"
-scp -r dist/ ${DEPLOY_USER}@${DEPLOY_SERVER}:/var/www/html/${SERVER_FOLDER}/
+echo "Deploying to ${DEPLOY_SERVER} - environment: ${environment_name}"
+scp -r dist/* ${DEPLOY_USER}@${DEPLOY_SERVER}:${SERVER_FOLDER}/
 
 echo "Finished copying the build files"

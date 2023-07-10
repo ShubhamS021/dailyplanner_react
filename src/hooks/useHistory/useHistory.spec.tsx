a6f3ebdc0d56306a88b27a96014c164db06d7dd1
ShubhamS021 <shubham@urbanoinfotech.com>
2023-07-10 09:05:00 +0200

    test: useHistory tests

---
+++ src/hooks/useHistory/useHistory.spec.tsx
@@ -0,0 +1,28 @@
+import { initDB } from 'utils/indexdb.util';
+import useHistory from './useHistory';
+import { act, renderHook } from '@testing-library/react';
+import { useBoardStore } from 'hooks/useBoardStore/useBoardStore';
+import { initialBoardState } from 'hooks/useBoardStore/data/initialBoard.state';
+import { initialLanes } from 'hooks/useBoardStore/data/initialLanes.state';
+
+describe('useHistory', () => {
+    // add a default board with some columns
+    beforeEach(() => {
+        void initDB();
+        const { result } = renderHook(() => useBoardStore());
+
+        act(() => {
+            const boardId = 0;
+            result.current.addBoard({
+                ...initialBoardState,
+                lanes: [...initialLanes],
+                id: boardId,
+            });
+        });
+    });
+
+    test('gets the history for board', () => {
+        const { result } = renderHook(() => useHistory(1));
+        expect(result.current.history).toBeDefined();
+    });
+});

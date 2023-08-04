aa6a6e56c53caf65014b0d3cdf765be6614f7c4e
ShubhamS021 <shubham@urbanoinfotech.com>
2023-08-04 22:58:16 +0200

    fix: fixing typo in setup

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: './src/setUpTests.ts',
+        setupFiles: './src/setupTests.ts',
         coverage: {
             provider: 'v8',
         },

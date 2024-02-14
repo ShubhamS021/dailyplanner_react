e8914ad720c6dc3acaaf698a3446ec9e40232069
ShubhamS021 <shubham@urbanoinfotech.com>
2024-02-14 15:32:08 +0100

    feat: rework history to shadcn table

--- src/components/BoardHistory/BoardHistory.tsx
+++ src/components/BoardHistory/BoardHistory.tsx
@@ -6,6 +6,8 @@ import { usePageStore } from '@/hooks/usePageStore/usePageStore';
 import { ArrowLeftIcon } from '@/ui/Icons/Icons';
 import { useTranslation } from 'react-i18next';
 
+import { Table, TableBody, TableCell, TableHeader, TableRow } from '@/ui/table';
+
 export const BoardHistory = () => {
     const [board] = useBoardStore((state) => [state.board]);
 
@@ -46,35 +48,35 @@ export const BoardHistory = () => {
     const renderHistoryTable = () => {
         return (
             <div className="relative overflow-x-auto">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
-                        <tr>
-                            <th scope="col" className="px-6 pb-4">
+                <Table className="w-full text-sm text-left">
+                    <TableHeader className="text-xs uppercase">
+                        <TableRow>
+                            <TableCell scope="col" className="px-6 pb-4">
                                 {t('components.BoardHistory.time')}
-                            </th>
-                            <th scope="col" className="pb-4">
+                            </TableCell>
+                            <TableCell scope="col" className="pb-4">
                                 {t('components.BoardHistory.type')}
-                            </th>
-                            <th scope="col" className="pb-4">
+                            </TableCell>
+                            <TableCell scope="col" className="pb-4">
                                 {t('components.BoardHistory.data')}
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                            </TableCell>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
                         {history.map((h) => {
                             return (
-                                <tr
+                                <TableRow
                                     key={h.id}
                                     className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                                     data-testid={h.type}
                                 >
-                                    <th
+                                    <TableCell
                                         scope="row"
                                         className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                                     >
                                         {new Date(h.id).toLocaleString()}
-                                    </th>
-                                    <td className="py-4">
+                                    </TableCell>
+                                    <TableCell className="py-4">
                                         {t(
                                             `components.BoardHistory.` +
                                                 h.type.toLowerCase()
@@ -84,15 +86,15 @@ export const BoardHistory = () => {
                                         {`${t(
                                             `components.BoardHistory.additionaldata`
                                         )}: ${renderAdditionalData(h)}`}
-                                    </td>
-                                    <td className="py-4 max-w-[20vw]">
+                                    </TableCell>
+                                    <TableCell className="py-4 max-w-[20vw]">
                                         {JSON.stringify(h.data.card)}
-                                    </td>
-                                </tr>
+                                    </TableCell>
+                                </TableRow>
                             );
                         })}
-                    </tbody>
-                </table>
+                    </TableBody>
+                </Table>
             </div>
         );
     };
@@ -118,10 +120,7 @@ export const BoardHistory = () => {
                     }
                 />
             </div>
-            <div
-                className={`p-5 rounded-2xl bg-[#F8F8F8] grid gap-6 dark:bg-[#212932]`}
-                data-testid="page-board"
-            >
+            <div className={`grid gap-6`} data-testid="page-board">
                 <div className="table">
                     {history.length === 0
                         ? renderNoData()

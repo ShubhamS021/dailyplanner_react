1912b4f89539ff8a9e9a59990787a00cdc6e21d8
ShubhamS021 <shubham@urbanoinfotech.com>
2024-02-17 18:57:19 +0100

    feat: reworked compact-mode

--- src/components/toggles/compact-mode/compact-mode.tsx
+++ src/components/toggles/compact-mode/compact-mode.tsx
@@ -1,5 +1,6 @@
 import { useBoardStore } from '@/hooks/useBoardStore/useBoardStore';
-import { CompactModeIcon } from '@/ui/Icons/Icons';
+import { Button } from '@/ui/button';
+import { KanbanIcon, LayoutDashboard } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
 export const CompactMode = () => {
@@ -11,26 +12,25 @@ export const CompactMode = () => {
     const { t } = useTranslation();
 
     return (
-        <>
-            <button
-                className="hover:text-[#17A2B8] font-semibold dark:text-[#B5B5B5] dark:hover:text-[#17A2B8]"
-                data-testid="compactmode-toggle-button"
-                onClick={(_e) => {
-                    toggleCompactMode();
-                }}
-            >
-                <div className="flex gap-2 items-center p-2 stroke-[#5E5E5E] dark:stroke-[#B5B5B5] dark:hover:stroke-[#17A2B8] hover:stroke-[#17A2B8] soft">
-                    <CompactModeIcon
-                        viewBox={{ x: 0, y: 0, width: 18, height: 18 }}
-                    />
-                    <p className="font-semibold text-sm">
-                        {compactMode
-                            ? t('components.CompactModeToggle.normal')
-                            : t('components.CompactModeToggle.compact')}
-                    </p>
-                </div>
-            </button>
-        </>
+        <Button
+            data-testid="compactmode-toggle-button"
+            variant={'outline'}
+            size={'sm'}
+            onClick={(_e) => {
+                toggleCompactMode();
+            }}
+        >
+            <div className="flex gap-2 items-center">
+                {compactMode ? (
+                    <LayoutDashboard width={16} height={16} />
+                ) : (
+                    <KanbanIcon width={16} height={16} />
+                )}
+                {compactMode
+                    ? t('components.CompactModeToggle.normal')
+                    : t('components.CompactModeToggle.compact')}
+            </div>
+        </Button>
     );
 };
 

c251fc0920d1efd0766e46d2617deae030e31734
ShubhamS021 <shubham@urbanoinfotech.com>
2024-02-18 19:50:38 +0100

    test: try to fixup the location issue by mocks

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,14 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import { afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
 import { DEtranslation } from '@/assets/i18n/de';
 import { ENtranslation } from '@/assets/i18n/en';
-import 'fake-indexeddb/auto';
 import i18n from '@/i18n';
+import '@testing-library/jest-dom';
+import { cleanup } from '@testing-library/react';
+import 'fake-indexeddb/auto';
 import { initReactI18next } from 'react-i18next';
+import { afterEach } from 'vitest';
 
 if (!i18n.isInitialized) {
     i18n.use(initReactI18next)
@@ -27,6 +27,11 @@ if (!i18n.isInitialized) {
         })
         .finally(() => {});
 }
+
+const original = window.location;
+beforeEach(() => {
+    window.location = original;
+});
 
 // Run cleanup after each test case (e.g., clearing jsdom)
 afterEach(() => {

f571fa95ce94f9f401791a02af2bb532ffb5bd3c
ShubhamS021 <shubham@urbanoinfotech.com>
2024-02-20 11:52:34 +0100

    chore: cleanup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,10 +1,10 @@
+import { DEtranslation } from '@/assets/i18n/de';
+import { ENtranslation } from '@/assets/i18n/en';
+import i18n from '@/i18n';
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import { DEtranslation } from '@/assets/i18n/de';
-import { ENtranslation } from '@/assets/i18n/en';
-import i18n from '@/i18n';
 import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import 'fake-indexeddb/auto';
@@ -27,11 +27,6 @@ if (!i18n.isInitialized) {
         })
         .finally(() => {});
 }
-
-const original = window.location;
-beforeEach(() => {
-    window.location = original;
-});
 
 // Run cleanup after each test case (e.g., clearing jsdom)
 afterEach(() => {

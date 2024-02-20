7a7eebc4e5c7ffe6a8e3f238ed4b061d57609c2d
ShubhamS021 <shubham@urbanoinfotech.com>
2024-02-20 14:34:18 +0100

    test: exclude files from test

--- src/ui/dropzone.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import Dropzone from './dropzone';
-
-test('renders basic dropzone', () => {
-    const { getByTestId } = render(<Dropzone text="Place tasks here..." />);
-    expect(getByTestId(/dropzone/i).textContent).toBe('Place tasks here...');
-});

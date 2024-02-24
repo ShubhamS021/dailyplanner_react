3ac5ec378c1f43e11534071d6b85a3c1f2b7ff1f
ShubhamS021 <shubham@urbanoinfotech.com>
2024-02-24 16:46:19 +0100

    feat: indicator at compact mode

--- src/components/toggles/compact-mode/compact-mode.tsx
+++ src/components/toggles/compact-mode/compact-mode.tsx
@@ -12,25 +12,33 @@ export const CompactMode = () => {
     const { t } = useTranslation();
 
     return (
-        <Button
-            data-testid="compactmode-toggle-button"
-            variant={'outline'}
-            size={'xs'}
-            onClick={(_e) => {
-                toggleCompactMode();
-            }}
-        >
-            <div className="flex gap-1 items-center text-xs">
-                {compactMode ? (
-                    <LayoutDashboard width={14} height={14} />
-                ) : (
-                    <KanbanIcon width={14} height={14} />
-                )}
-                {compactMode
-                    ? t('components.CompactModeToggle.normal')
-                    : t('components.CompactModeToggle.compact')}
-            </div>
-        </Button>
+        <span className="relative inline-flex">
+            <Button
+                data-testid="compactmode-toggle-button"
+                variant={'outline'}
+                size={'xs'}
+                onClick={(_e) => {
+                    toggleCompactMode();
+                }}
+            >
+                <div className="flex gap-1 items-center text-xs">
+                    {compactMode ? (
+                        <LayoutDashboard width={14} height={14} />
+                    ) : (
+                        <KanbanIcon width={14} height={14} />
+                    )}
+                    {compactMode
+                        ? t('components.CompactModeToggle.normal')
+                        : t('components.CompactModeToggle.compact')}
+                </div>
+            </Button>
+            {compactMode && (
+                <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
+                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-lavender opacity-75"></span>
+                    <span className="relative inline-flex rounded-full h-3 w-3 bg-lavender"></span>
+                </span>
+            )}
+        </span>
     );
 };
 

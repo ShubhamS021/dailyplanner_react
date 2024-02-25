43d87e7cdc20f494f66efbac90ad14d4753ca6a6
ShubhamS021 <shubham@urbanoinfotech.com>
2024-02-25 10:18:20 +0100

    ci: setup env in before script ci: cleanup of used variables ci: extract setup in own bash file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
 # Environment variables
-environment_name=$ENVIRONMENT_NAME
 DEPLOY_SERVER=$DEPLOY_SERVER
 DEPLOY_USER=$DEPLOY_USER
 
-# .env contents
-VITE_SUPABASE_URL=$VITE_SUPABASE_URL
-VITE_SUPABASE_ANON_KEY=$VITE_SUPABASE_ANON_KEY
-VITE_PUBLIC_SITE_URL=$CI_ENVIRONMENT_URL
 
-if [[ "$environment_name" == "qa" ]]; then
+if [[ "$CI_ENVIRONMENT_NAME" == "qa" ]]; then
   SERVER_FOLDER="httpdocs/apps-qa/plannerapp"
-elif [[ "$environment_name" == "prod" ]]; then
+elif [[ "$CI_ENVIRONMENT_NAME" == "prod" ]]; then
   SERVER_FOLDER="httpdocs/apps/plannerapp"
 else
-  echo "Invalid environment name: $environment_name"
+  echo "Invalid environment name: $CI_ENVIRONMENT_NAME"
   exit 1
 fi
 
 yarn install
 yarn build
 
-ENV_FILE=dist/.env
-touch $ENV_FILE
-echo "$VITE_SUPABASE_URL" > "$ENV_FILE"
-echo "$VITE_SUPABASE_ANON_KEY" > "$ENV_FILE"
-echo "$VITE_PUBLIC_SITE_URL" > "$ENV_FILE"
-
-echo "Deploying to ${DEPLOY_SERVER} - environment: ${environment_name}"
+echo "Deploying to ${DEPLOY_SERVER} - environment: ${$CI_ENVIRONMENT_NAME}"
 scp -r dist/* ${DEPLOY_USER}@${DEPLOY_SERVER}:${SERVER_FOLDER}/
+scp -r .env ${DEPLOY_USER}@${DEPLOY_SERVER}:${SERVER_FOLDER}/
 echo "Finished copying the build files"
 
 
